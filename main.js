64364f19b8102a609445497fa75901851e6670d5
Timur-V90 <timur.fumoff@gmail.com>
2026-01-20 12:42:28 +0300

    feat(header):add a drop-down menu layout

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const unitsBtn = document.querySelector("[data-js-units-button]");
-const headerDropdown = document.querySelector("[data-js-header-dropdown]");
+const unitsBtnElement = document.querySelector("[data-js-units-button]");
+const headerDropdownElement = document.querySelector("[data-js-header-dropdown]");
 
 //открытие и закрытие dropdown по клику на кноку
-unitsBtn.addEventListener('click', function(e) {
+unitsBtnElement.addEventListener('click', function(e) {
   e.stopPropagation();
-  headerDropdown.classList.toggle('dropdown-visible');
+  headerDropdownElement.classList.toggle('dropdown-visible');
 });
 
 
 //закрытие dropdown по клику в любое место документа вне dropdown
 document.addEventListener('click', function(e) {
   if (!e.target.classList.contains('dropdown')) {
-    headerDropdown.classList.remove('dropdown-visible');
+    headerDropdownElement.classList.remove('dropdown-visible');
   }
 })
 
 //закрытие dropdown по клавише escape
 document.addEventListener('keydown', function(e) {
-  if (e.key === 'Escape' || headerDropdown.classList.contains('dropdown-visible')) {
-    headerDropdown.classList.remove('dropdown-visible');
+  if (e.key === 'Escape' || headerDropdownElement.classList.contains('dropdown-visible')) {
+    headerDropdownElement.classList.remove('dropdown-visible');
   }
 })
