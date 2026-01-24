af997bf61b3be34f360dc7b5128536da503ca6a2
Timur-V90 <timur.fumoff@gmail.com>
2026-01-24 20:48:12 +0300

    feat(search): add the auto-complete functionality from the drop-down list

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const unitsBtnElement = document.querySelector("[data-js-units-button]");
 const headerDropdownElement = document.querySelector("[data-js-header-dropdown]");
+const inputElement = document.querySelector("[data-js-search-input]");
+const autocompleteDropdownElement = document.querySelector("[data-js-search-autocomplete-dropdown]");
+const autocompleteListElement = document.querySelector("[data-js-autocomplete-list]");
 
+
+//header-dropdown
 //открытие и закрытие dropdown по клику на кноку
 unitsBtnElement.addEventListener('click', function(e) {
   e.stopPropagation();
@@ -19,5 +24,32 @@ document.addEventListener('click', function(e) {
 document.addEventListener('keydown', function(e) {
   if (e.key === 'Escape' || headerDropdownElement.classList.contains('dropdown-visible')) {
     headerDropdownElement.classList.remove('dropdown-visible');
+  }
+})
+
+//search input dropdown
+//показ списка городов автозаполнения
+inputElement.addEventListener('focus', function() {
+  autocompleteDropdownElement.classList.add('dropdown-visible');
+})
+
+//скрытие списка городов автозаполнения
+inputElement.addEventListener('blur', function() {
+  autocompleteDropdownElement.classList.remove('dropdown-visible');
+})
+
+autocompleteListElement.addEventListener('mousedown', function(e) {
+  if(e.target.matches('.search__autocomplete-item')) {
+    e.preventDefault(); // Предотвращаем blur на инпуте
+  }
+});
+
+
+autocompleteListElement.addEventListener('click', function(e) {
+  if(e.target.matches('.search__autocomplete-item')) {
+    e.stopPropagation();
+    inputElement.value = e.target.textContent;
+    autocompleteDropdownElement.classList.remove('dropdown-visible');
+    inputElement.blur();
   }
 })
