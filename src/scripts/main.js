cdba526c061282da9e42a3a4494656e13b92b669
Timur-V90 <timur.fumoff@gmail.com>
2026-01-27 16:06:52 +0300

    feat(search): decompose javascript files into modules

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+import { initHeaderUnitsButton } from './modules/header-units-button.js';
+import { initSearchCityInput } from './modules/search-city-input.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+  initHeaderUnitsButton();
+  initSearchCityInput();
+});
+
